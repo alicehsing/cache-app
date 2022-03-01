04c310d2c9b79f901bb2691facf7f0a69444dd1e
Pete Seeling <peteseeling@gmail.com>
2022-03-01 14:27:46 -0600

    Cache detail pseudo code complete

--- src/CacheProvider.js
+++ src/CacheProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext } from 'react';
-import { client } from './services/client';
+// import { client } from './services/client';
 import { signInUser, signUpUser } from './services/fetch-utils';
 
 const CacheContext = createContext();
--- src/Components/Main/Cache/CachePage.js
+++ src/Components/Main/Cache/CachePage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useCacheContext } from '../../../CacheProvider';
 
 export default function CachePage() {
+  const { search, setSearch, cacheList, setCacheList, userLocation, setUserLocation, toggleView, setToggleView } = useCacheContext();
   return (
     <>
-        
+      {/* carousoul, search and toggle view 
+      true= map
+      false= carousoul
+      onClick link to detail page*/}
     </>
   );
 }
