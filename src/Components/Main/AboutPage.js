7e3422f8c3b54fe9cd0c1fe487373ffe600820f3
Pete Seeling <peteseeling@gmail.com>
2022-03-01 14:43:53 -0600

    about page psuedo code complete

--- src/Components/Main/AboutPage.js
+++ src/Components/Main/AboutPage.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 export default function About() {
   return (
+
+    // Carasoul-dev picture and description
+    // create dev profile data somewhere
     <>
     
     </>
