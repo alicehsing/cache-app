9f510bd1362a2ea3ba2a27c167eeeead202a0ba8
Alice Hsing <alicehsing@gmail.com>
2022-03-02 12:55:15 -0800

    added link to cache item

--- src/Components/Main/Cache/CacheItem.js
+++ src/Components/Main/Cache/CacheItem.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useCacheContext } from '../../../CacheProvider';
 
 export default function CacheItem({ cache }){
     // render item information. 
-  console.log(cache);
   return (
     <>
-      
-      <figure>
-        <h2>{cache.title}</h2>
-        <img alt={cache.title} src={cache.image}/>
-        <figcaption>{cache.latitude}°, {cache.longitude}°</figcaption>
-      </figure>
+      <Link to={`/cachelist/${cache.id}`}>
+        <figure>
+          <h2>{cache.title}</h2>
+          <img alt={cache.title} src={cache.image}/>
+          <figcaption>{cache.latitude}°, {cache.longitude}°</figcaption>
+        </figure>
+      </Link>
     </>
   );
 }
