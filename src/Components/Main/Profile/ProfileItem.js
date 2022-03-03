1be5abdd45772fa7979fba3cdbbd8ef2eacd1e44
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-03-03 15:26:39 -0800

    added info to profileitem

--- src/Components/Main/Profile/ProfileItem.js
+++ src/Components/Main/Profile/ProfileItem.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-export default function ProfileItem() {
+export default function ProfileItem({ cache }) {
+    
   return (
-    <div>ProfileItem</div>
+    <figure id='figure-detail'>
+      <h2 id='cache-title'>{cache.title}</h2>
+      <img id='item-image' alt={cache.title} src={cache.image}/>
+      <figcaption id='lat-lon'>{cache.latitude}°, {cache.longitude}°</figcaption>
+    </figure>
   );
 }
