780ae244299dc5a57d4d45cfe8fd033db12bce8e
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-03-03 09:38:46 -0800

    added about-data file

---
+++ src/about-data.js
@@ -0,0 +1,22 @@
+export const aboutData = [
+  {
+    name: 'Alice Hsing',
+    image: src,
+    description: `This is Alice's example description`,
+  },
+  {
+    name: 'Ryan Flitcroft',
+    image: src,
+    description: `This is Ryan's example description`,
+  },
+  {
+    name: 'Pete Seeling',
+    image: src,
+    description: `This is Pete's example description`,
+  },
+  {
+    name: 'Ari Harlem-Caballero',
+    image: src,
+    description: `This is Ari's example description`
+  }
+];
