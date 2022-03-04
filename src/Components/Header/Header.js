81b831cff662c718325cb09b7b663e3f06739f85
Pete Seeling <peteseeling@gmail.com>
2022-03-03 18:17:03 -0600

    css almost complete. need background images

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,14 @@
+
 import React from 'react';
 import logo from './logo.png';
+
 export default function Header() {
 
   return (
-    <header id='main-header'>
+    <header id='main-header' >
+     
       <img src={logo} alt='logo' id='logo-img' />
+
     </header>
   );
 }
