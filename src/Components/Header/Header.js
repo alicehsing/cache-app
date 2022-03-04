489589ee854eb9d3588b0d0ec8434c6aa3a774b7
Pete Seeling <peteseeling@gmail.com>
2022-03-03 19:27:14 -0600

    header map pic rendering properly

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,11 @@
 
 import React from 'react';
 import logo from './logo.png';
-
+import background from './background-3.png';
 export default function Header() {
 
   return (
-    <header id='main-header' >
-     
+    <header id='main-header' style={{ backgroundImage  : `url(${background})` }} >
       <img src={logo} alt='logo' id='logo-img' />
 
     </header>
